e1471304d95f09dcd4e672ddbb7bf50862870782
VARSHAN K N <varshanvarshan63@gmail.com>
2026-07-13 20:31:56 +0530

    initaial commit

---
+++ Day2.py
@@ -0,0 +1,23 @@
+name="Varshan"#string
+age=20#integer
+is_student=True#boolean
+weight=59.2#float
+
+print("My name is",name)
+print("I am",age,"years old")
+print("I am a student:",is_student)
+print("My weight is",weight,"kg")
+
+
+a=10
+b=5
+
+print("The sum of a and b is:",a+b)
+print("The difference of a and b is:",a-b)
+print("The product of a and b is:",a*b)         
+print("The division of a and b is:",a/b)
+print("The modulus of a and b is:",a%b)
+print("The floor division of a and b is:",a//b)
+print("The exponentiation of a and b is:",a**b)
+
+print("a+b=",a+b)
