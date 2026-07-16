8d5295bf7c42ae34f1df187935cf82aded9eaec7
VARSHAN K N <varshanvarshan63@gmail.com>
2026-07-16 21:52:09 +0530

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,41 @@
+#list
+#list is the collection of items
+
+#my_list = [_element 1,element 2,element 3"]
+
+fruits=["apple","banana","cherry"]
+numbers=[1,2,3,4,5]
+mixed = [ "apple",3,True ]
+print(fruits)
+print(numbers)
+print(mixed)
+
+#accessing list elements
+#You can access individual elements in a list using indexing. Remember that Python uses zero-based indexing, so the first item is at index 0.
+
+fruits = ["apple","banana","cherry"]
+print(fruits)
+
+fruits = ["apple","banana","cherry"]
+print = (fruits[0])#output:
+print(fruits)
+
+
+
+fruits = ["apple","banana","cherry"]
+print = (fruits[0])#output:apple
+print = (fruits[-1])#output:cherry
+
+#modifing lists
+#Lists are mutable, which means you can change the value of items in a list.
+
+
+fruits[1] = "orange"
+print(fruits)  # Output: ['apple', 'orange', 'cherry']
+
+
+#adding
+
+#1.append()
+fruits.append=("grape")
+print(fruits)
