edf1f53116f342d4544a780bec9d5f79efc98631
VARSHAN K N <varshanvarshan63@gmail.com>
2026-07-24 22:08:29 +0530

    Add files via upload

---
+++ Day11.py
@@ -0,0 +1,10 @@
+#3
+def marriage(boy, girl):
+    print(f"boy is {boy}")
+    print(f"girl is {girl}")
+    print(f"{boy} married {girl}")
+
+
+# Function calls outside the definition
+marriage("chandhan", "shreya")   # positional arrangement
+marriage(boy="chandhan", girl="chandana") # keyword arrangement
